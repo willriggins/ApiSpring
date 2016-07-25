b82a6f7665aa7f70113a51a5f83cedb96bbc3fec
willriggins <williamriggins2013@gmail.com>
2016-07-25 10:51:05 -0400

    add threading

--- src/main/java/com/theironyard/ApiController.java
+++ src/main/java/com/theironyard/ApiController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import javax.annotation.PostConstruct;
 import java.util.HashMap;
 
 /**
@@ -13,6 +14,26 @@ import java.util.HashMap;
 @RestController
 public class ApiController {
     static final String API_URL = "http://gturnquist-quoters.cfapps.io/api/random";
+
+    @PostConstruct
+    public void init() {
+
+
+        Thread t = new Thread() {
+            @Override
+            public void run() {
+                while (true) {
+                    System.out.println("Hello");
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        };
+        t.start();
+    }
 
     @RequestMapping(path = "/quote", method = RequestMethod.GET)
     public HashMap getQuote() {
