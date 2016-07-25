b81255dfc6bd7a34fde331076b61dbfa0c9f01aa
willriggins <williamriggins2013@gmail.com>
2016-07-25 11:03:06 -0400

    Add async method

--- src/main/java/com/theironyard/ApiController.java
+++ src/main/java/com/theironyard/ApiController.java
@@ -1,12 +1,17 @@
 package com.theironyard;
 
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.AsyncResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import javax.annotation.PostConstruct;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
 
 /**
  * Created by will on 7/25/16.
@@ -34,16 +39,36 @@ public class ApiController {
         };
         t.start();
     }
-
-    @RequestMapping(path = "/quote", method = RequestMethod.GET)
-    public HashMap getQuote() {
+    @Async
+    public Future<HashMap> requestQuote() {
+        // see more complex example here:
+        // https://github.com/Book-It/BookIt (get exact link for example from tiy chs link fork)
         RestTemplate query = new RestTemplate();
         HashMap result = query.getForObject(API_URL, HashMap.class);
         String type = (String) result.get("type");
         if (type.equals("success")) {
             HashMap value = (HashMap) result.get("value");
-            return value;
+            return new AsyncResult<>(value);
         }
+        return null;
+    }
+
+    @RequestMapping(path = "/quote", method = RequestMethod.GET)
+    public ArrayList getQuote() throws InterruptedException, ExecutionException {
+        Future<HashMap> quote1 = requestQuote();
+        Future<HashMap> quote2 = requestQuote();
+        Future<HashMap> quote3 = requestQuote();
+
+        while (!quote1.isDone() || !quote2.isDone() || !quote3.isDone()) {
+            Thread.sleep(100);
+        }
+
+        ArrayList arr = new ArrayList();
+        arr.add(quote1.get());
+        arr.add(quote2.get());
+        arr.add(quote3.get());
+
+
         return null;
     }
 }
--- src/main/java/com/theironyard/ApiSpringApplication.java
+++ src/main/java/com/theironyard/ApiSpringApplication.java
@@ -2,8 +2,10 @@ package com.theironyard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class ApiSpringApplication {
 
 	public static void main(String[] args) {
