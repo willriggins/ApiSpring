3b5c8429d0b5df0f74f04e775c69a021fe93fbb3
willriggins <williamriggins2013@gmail.com>
2016-07-25 10:33:24 -0400

    simple RestTemplate example

--- src/main/java/com/theironyard/ApiController.java
+++ src/main/java/com/theironyard/ApiController.java
@@ -1,7 +1,28 @@
 package com.theironyard;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+
 /**
  * Created by will on 7/25/16.
  */
+@RestController
 public class ApiController {
+    static final String API_URL = "http://gturnquist-quoters.cfapps.io/api/random";
+
+    @RequestMapping(path = "/quote", method = RequestMethod.GET)
+    public HashMap getQuote() {
+        RestTemplate query = new RestTemplate();
+        HashMap result = query.getForObject(API_URL, HashMap.class);
+        String type = (String) result.get("type");
+        if (type.equals("success")) {
+            HashMap value = (HashMap) result.get("value");
+            return value;
+        }
+        return null;
+    }
 }
